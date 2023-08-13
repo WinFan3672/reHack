c74cc9e2d630b09ec35fbbc43c5ecc7917320537
WinFan3672 <winfan3672@gmail.com>
2023-08-13 22:48:31 +0100

    Update __init__.py

--- resource/information/__init__.py
+++ resource/information/__init__.py
@@ -1,6 +1,6 @@
-version = [0,0,0]
+version = [0,0,1]
 friendlyVersion = ".".join([str(x) for x in version])
 authors = ["WinFan3672"]
 contributors = ["WinFan3672"]
-releaseDate = {"day":1,"month":"January","year":1970}
+releaseDate = {"day":13,"month":"August","year":2023}
 friendlyRelDate = "{} {} {}".format(releaseDate["day"],releaseDate["month"],releaseDate["year"])
