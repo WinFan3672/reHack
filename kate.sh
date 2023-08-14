6f6ff9ed7f0798b77b0f046e51c022593ff829ae
WinFan3672 <winfan3672@gmail.com>
2023-08-14 13:41:34 +0100

    Update kate.sh

--- kate.sh
+++ kate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
-kate rehack.py game/*/__init__.py resource/*/__init__.py data/__init__.py # data/*/__init__.py
+kate rehack.py game/*/__init__.py resource/*/__init__.py data/__init__.py game/programs/*/__init__.py
+# data/*/__init__.py
 github-desktop
