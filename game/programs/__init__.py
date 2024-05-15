caf647a0bc9410e2913fcb7ea469f069b100709b
Gordinator <gordinator@gordinator.org>
2024-05-15 17:00:30 +0100

    Fixed HostKillMission
    Made it so that the call to data.getAnyNode() in HostKillMission is
    stict, which fixes the bug where it didn't work.

--- game/programs/__init__.py
+++ game/programs/__init__.py
@@ -3882,7 +3882,8 @@ def date(args, player):
 
 class HostKillMission(Mission):
     def check_end(self):
-        node = data.getAnyNode(self.target)
+        node = data.getAnyNode(self.target, True)
+        print(node)
         return not node
 
 def openftp(args):
